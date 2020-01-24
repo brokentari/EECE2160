0da81b7d929ec3f43ae1aa3a72d090d2bdebba37
Saul Reyna <saul.reyna90@gmail.com>
2020-01-24 17:44:13 -0500

    sign commit test

--- Homeworks/Homework 1/Homework1_1.cpp
+++ Homeworks/Homework 1/Homework1_1.cpp
@@ -3,9 +3,10 @@
 
 using namespace std;
 
-int to_power_of(int n);
+int to_power_of(int n1, int n2);
 bool check_max(int n1, int n2);
 
 int main() {
     cout << "Hello World!" << endl;
+    cout << to_power_of(2, 3);
 }
