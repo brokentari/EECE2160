a275eef6ee62de6650ff90696aea0c652b6a93fe
brokentari <saul.reyna90@gmail.com>
2020-01-25 01:59:43 -0500

    final homework 1-1

--- Homeworks/Homework 1/Homework1_1.cpp
+++ Homeworks/Homework 1/Homework1_1.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <math.h>
 #include <algorithm>
+#include <bitset>
 
 using namespace std;
 
@@ -20,17 +21,47 @@ void dec2Hex(int n) {
     char hex[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 
     while(n > 0) {
-        temp = n % 16;;
+        temp = n % 16;
         conv = hex[temp] + conv;
         n = n / 16;
     }
 
-    cout << conv << endl;
+    cout << "Hexadecimal: " << conv << endl;
+}
+
+void dec2Oct(int n) {
+    string conv = "";
+    int temp;
+
+    while(n > 0) {
+        temp = n % 8;
+        conv = to_string(temp) + conv;
+        n = n / 8;
+    }
+
+    cout << "Octal: " << conv << endl;
+}
+
+void dec2Bin(int n) {
+    bitset<16> bits;
+    int temp;
+
+    for (int i = 0; i < 16; i++) {
+        temp = n % 2;
+        if(temp == 1) {
+            bits.set(i);
+        }
+        n = n / 2;
+    }
+
+    cout << "Binary: " << bits.to_string() << endl;
 }
 
 void convert(int n1) {
     cout << "Decimal: " << n1 << endl;
     dec2Hex(n1);
+    dec2Oct(n1);
+    dec2Bin(n1);
 }
 
 int main() {
