2ebfda3ce31c5ddab1de254cee8378980827d755
Saul Reyna <saul.reyna90@gmail.com>
2020-01-26 16:00:08 -0500

    lab 2 prelab

---
+++ Labs/Lab 2/person.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Person 
+{
+    string name;
+    int age;
+};
+
+void PrintPerson(Person *person) 
+{
+    cout << person->name << " is " << person->age << " years old\n";
+}
+
+int main() 
+{
+    Person person;
+    person.name = "John";
+    person.age = 10;
+    PrintPerson(&person);
+}
